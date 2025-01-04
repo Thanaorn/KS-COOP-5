07bfde1f5f192b9ac0a7a25de86fe0a2cc612bbc
Thanatorn <thanatornsense2468@gmail.com>
2025-01-04 18:04:06 +0700

    env fix

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module teach
 go 1.23.4
 
 require (
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo v3.3.10+incompatible
 	go.mongodb.org/mongo-driver v1.17.1
 )
--- main.go
+++ main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"teach/connector"
 	"teach/webhook/router"
 	"teach/webhook/service"
 
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	e := echo.New()
+
+	wd, _ := os.Getwd()
+	if err := godotenv.Load(wd + "/.env"); err != nil {
+		fmt.Println("Error loading .env file.")
+	}
+	fmt.Println("uri = ", os.Getenv("MONGODB_URI"))
 
 	mongodb := connector.NewMongoDBClient(os.Getenv("MONGODB_URI"), 100)
 	if mongodb == nil {
