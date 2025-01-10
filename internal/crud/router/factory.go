5fa835b151ee6aa56d40e5f077a981c6c1b663ba
Thanatorn <thanatornsense2468@gmail.com>
2025-01-10 19:24:22 +0700

    Remove return router

--- internal/crud/router/factory.go
+++ internal/crud/router/factory.go
@@ -7,21 +7,8 @@ import (
 )
 
 type CrudRouters struct {
-	CrudService cs.ICrudService
 }
 
 func NewCrudRouter(e *echo.Echo, cs cs.ICrudService) {
-
-	crud := CrudRouters{
-		CrudService: cs,
-	}
-
-	crudTestApi := e.Group("/test")
-
-	crudTestApi.GET("/readData", crud.readData)
-	crudTestApi.GET("/readData/:id", crud.readDataId)
-	crudTestApi.POST("/createData", crud.createData)
-	crudTestApi.POST("/updateData", crud.updateData)
-	crudTestApi.DELETE("/deleteData/:id", crud.deleteData)
 
 }
